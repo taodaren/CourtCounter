4e9697f9a7a14453622e47e5c301a8253a6700a0
taodaren <taodaren1125@gmail.com>
2017-12-07 16:46:31 +0800

    设置方法

--- app/src/main/java/io/github/taodaren/courtcounter/MainActivity.java
+++ app/src/main/java/io/github/taodaren/courtcounter/MainActivity.java
@@ -1,13 +1,42 @@
 package io.github.taodaren.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_1).setOnClickListener(this);
+        findViewById(R.id.btn_2).setOnClickListener(this);
+        findViewById(R.id.btn_3).setOnClickListener(this);
     }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_1:
+                displayForTeamA(1);
+                break;
+            case R.id.btn_2:
+                displayForTeamA(2);
+                break;
+            case R.id.btn_3:
+                displayForTeamA(3);
+                break;
+        }
+    }
+
 }
